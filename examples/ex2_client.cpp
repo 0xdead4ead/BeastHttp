c39c17409d28bb289bac1b5e75d19605e26e41db
0xdead4ead <steamtixonow26@gmail.com>
2018-07-25 17:30:06 +0500

    examples/ex2_client.cpp

---
+++ examples/ex2_client.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <client.hpp>
+
+using namespace std;
+
+
+int main()
+{
+//g++ -c -std=gnu++14 -I.. -o ex2_client.o ./ex2_client.cpp
+//g++ -o ex2_client ex2_client.o -lboost_system -lboost_thread -lpthread -lboost_regex -licui18n
+    http::client my_http_client;
+
+    const auto & host = "www.google.com";
+    const auto & port = 80;
+
+    const auto & on_connect = [&host](auto & session){
+        http::base::out("Successful connected!");
+
+        boost::beast::http::request<boost::beast::http::string_body> req;
+        req.version(11); // HTTP 1.1
+        req.method(boost::beast::http::verb::get); // GET
+        req.target("/");
+        req.set(boost::beast::http::field::host, host);
+        req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        session->do_write(boost::move(req));
+    };
+
+    const auto & on_receive = [](auto & res){
+        cout << *res << endl;
+        http::base::processor::get().stop();
+    };
+
+    my_http_client.get(host, port, on_connect, on_receive);
+
+
+    uint32_t pool_size = boost::thread::hardware_concurrency();
+    http::base::processor::get().start(pool_size == 0 ? 4 : pool_size << 1);
+    http::base::processor::get().wait();
+
+    return 0;
+}
