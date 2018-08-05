e22130fefa393e927c937215d8f7f1675ad98066
0xdead4ead <steamtixonow26@gmail.com>
2018-08-05 14:38:39 +0500

    new routing model, add chain processor

--- server.hpp
+++ server.hpp
@@ -8,286 +8,195 @@
 #include <base.hpp>
 #include <session.hpp>
 #include <list_cb.hpp>
+#include <router.hpp>
 
 
 namespace http {
 
-/// \brief Routing module class
+/// \brief HTTP server class
+/// \tparam Body type for request message
 template<class ReqBody>
-class router{
+class server_impl{
 
+    using basic_r = basic_router<ReqBody>;
+    using chain_r = chain_router<ReqBody>;
     using list_cb_t = list_cb<boost::beast::http::request<ReqBody>, session<true, ReqBody>>;
     using resource_map_t = boost::unordered_map<resource_regex_t,typename list_cb_t::ptr>;
     using method_map_t = std::map<method_t, resource_map_t>;
 
+    bool status_;
+    std::shared_ptr<basic_r> basic_router_;
+    std::shared_ptr<chain_r> chain_router_;
+    std::shared_ptr<resource_map_t> resource_map_cb_p_;
+    std::shared_ptr<method_map_t> method_map_cb_p_;
+
+    bool check_status(){
+        if(status_){
+            base::fail("Adding a handler is not available when the server is running");
+            return status_;
+        }
+        return status_;
+    }
+
+    const std::shared_ptr<basic_r> & get_basic_router(){
+        if(!basic_router_)
+            basic_router_ = std::make_shared<basic_r>(resource_map_cb_p_, method_map_cb_p_);
+
+        return basic_router_;
+    }
+
+    const std::shared_ptr<chain_r> & get_chain_router(){
+        if(!chain_router_)
+            chain_router_ = std::make_shared<chain_r>(resource_map_cb_p_, method_map_cb_p_);
+
+        return chain_router_;
+    }
+
 public:
 
-    using ref = std::add_lvalue_reference_t<router>;
-    using cref = std::add_lvalue_reference_t<std::add_const_t<router>>;
-
-    explicit router() :
+    explicit server_impl() :
         status_{false},
+        basic_router_{nullptr},
+        chain_router_{nullptr},
         resource_map_cb_p_{nullptr},
         method_map_cb_p_{nullptr}
     {}
 
-//    router(router_cref other){
-//        use(other);
-//    }
+    typename chain_r::ref route(const resource_regex_t & path_to_resource){
+        if(!check_status())
+            get_chain_router()->save_to_res_regex(path_to_resource);
 
-//    auto& operator=(router_cref other){
-//        use(other);
-//        return *this;
-//    }
-
-//    ~router() = default;
-
+        return get_chain_router().operator *();
+    }
     /// Callback signature : template<class Message, class Session /*, class Next (optional)*/>
     ///                     void (const Message & message, Session & session /*, Next & next (optional)*/)
     /// \brief Adds a handler for GET method
     template<class... Callback>
     void get(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::get, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->get(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for POST method
     template<class... Callback>
     void post(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::post, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->post(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for PUT method
     template<class... Callback>
     void put(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::put, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->put(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for HEAD method
     template<class... Callback>
     void head(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::head, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->head(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for DELETE method
     template<class... Callback>
     void delete_(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::delete_, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->delete_(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for OPTIONS method
     template<class... Callback>
     void options(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::options, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->options(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for CONNECT method
     template<class... Callback>
     void connect(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::connect, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->connect(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for TRACE method
     template<class... Callback>
     void trace(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::trace, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->trace(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for COPY method
     template<class... Callback>
     void copy(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::copy, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->copy(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for LOCK method
     template<class... Callback>
     void lock(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::lock, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->lock(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for MKCOL method
     template<class... Callback>
     void mkcol(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::mkcol, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->mkcol(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for MOVE method
     template<class... Callback>
     void move(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::move, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->move(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for PROPFIND method
     template<class... Callback>
     void propfind(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::propfind, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->propfind(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for PROPPATCH method
     template<class... Callback>
     void proppatch(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::proppatch, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->proppatch(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for SEARCH method
     template<class... Callback>
     void search(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::search, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->search(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for UNLOCK method
     template<class... Callback>
     void unlock(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::unlock, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->unlock(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for BIND method
     template<class... Callback>
     void bind(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::bind, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->bind(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for REBIND method
     template<class... Callback>
     void rebind(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::rebind, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->rebind(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for UNBIND method
     template<class... Callback>
     void unbind(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::unbind, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->unbind(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for ACL method
     template<class... Callback>
     void acl(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb(path_to_resource, method_t::acl, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->acl(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
     /// \brief Adds a handler for the requested resource by default
     /// \note If the handler for the requested resource with method is not found, this on is called
     template<class... Callback>
     void all(const resource_regex_t & path_to_resource, Callback && ... on_resource_handlers){
-        return add_resource_cb_without_method(path_to_resource, list_cb_t{prepare_list_cb(on_resource_handlers...)});
+        if(!check_status())
+            get_basic_router()->all(path_to_resource, std::forward<Callback>(on_resource_handlers)...);
     }
-
-private:
-
-    template <typename T, typename F1, typename F2, size_t Index = 0>
-    void tuple_cb_for_each(const T& tpl, const F1& f1, const F2& f2) {
-
-        constexpr auto tuple_size = std::tuple_size<T>::value;
-        BOOST_STATIC_ASSERT(tuple_size != 0);
-
-        const auto& value = std::get<Index>(tpl);
-        if constexpr(Index + 1 == tuple_size){
-            f1(value);
-        }else{
-            f2(value);
-            tuple_cb_for_each<T, F1, F2, Index + 1>(tpl, f1, f2);
-        }
-    }
-
-    template<class... Callback>
-    auto prepare_list_cb(const Callback & ... handlers){
-
-        typename list_cb_t::L cb_l;
-
-        auto tuple_cb = std::tie(handlers...);
-
-        tuple_cb_for_each(tuple_cb, [&cb_l](const auto& value){
-            cb_l.push_back(
-                        typename list_cb_t::F(
-                            boost::bind<void>(
-                                value,
-                                boost::placeholders::_1,
-                                boost::placeholders::_2
-                                )
-                            )
-                        );
-        }, [&cb_l](const auto& value){
-            cb_l.push_back(
-                        typename list_cb_t::F(
-                            boost::bind<void>(
-                                value,
-                                boost::placeholders::_1,
-                                boost::placeholders::_2,
-                                boost::placeholders::_3
-                                )
-                            )
-                        );
-        });
-
-        return cb_l;
-
-    }
-
-protected:
-
-    void add_resource_cb(const resource_regex_t & path_to_resource, const method_t & method, list_cb_t && l){
-        if(status_)
-            return base::fail("Adding a handler is not available when the server is running");
-
-        if(!method_map_cb_p_)
-            method_map_cb_p_ = std::make_shared<method_map_t>();
-
-        method_map_cb_p_->insert({
-                                     method,
-                                     resource_map_t()
-                                 });
-
-        auto & resource_map_cb = method_map_cb_p_->at(method);
-
-        resource_map_cb.insert({
-                                   path_to_resource,
-                                   typename list_cb_t::ptr()
-                               });
-
-        auto & list_cb_p = resource_map_cb.at(path_to_resource);
-        if(!list_cb_p)
-            list_cb_p = std::make_shared<list_cb_t>();
-
-        *list_cb_p = std::move(l);
-    }
-
-    void add_resource_cb_without_method(const resource_regex_t & path_to_resource, list_cb_t && l){
-        if(status_)
-            return base::fail("Adding a handler is not available when the server is running");
-
-        if(!resource_map_cb_p_)
-            resource_map_cb_p_ = std::make_shared<resource_map_t>();
-
-        resource_map_cb_p_->insert({
-                                       path_to_resource,
-                                       typename list_cb_t::ptr()
-                                   });
-
-        auto & list_cb_p = resource_map_cb_p_->at(path_to_resource);
-        if(!list_cb_p)
-            list_cb_p = std::make_shared<list_cb_t>();
-
-        *list_cb_p = std::move(l);
-    }
-
-    ref use(const resource_regex_t & path_to_resource, cref other){
-        if(other.resource_map_cb_p_)
-            for(const auto & value : *other.resource_map_cb_p_){
-                auto l_cb = *value.second;
-                add_resource_cb_without_method(path_to_resource + value.first, std::move(l_cb));
-            }
-
-
-        if(other.method_map_cb_p_)
-            for(const auto & value_m : *other.method_map_cb_p_){
-                auto method = value_m.first;
-                const auto & resource_map = value_m.second;
-
-                for(const auto & value_r : resource_map){
-                    auto l_cb = *value_r.second;
-                    add_resource_cb(path_to_resource + value_r.first, method,std::move(l_cb));
-                }
-            }
-        return *this;
-    }
-
-    bool status_;
-    std::shared_ptr<resource_map_t> resource_map_cb_p_;
-    std::shared_ptr<method_map_t> method_map_cb_p_;
-
-}; // router class
-
-/// \brief HTTP server class
-/// \tparam Body type for request message
-template<class ReqBody>
-class server_impl : public router<ReqBody>{
-
-public:
-
-    using router_base = router<ReqBody>;
-
-    explicit server_impl() :
-        router_base()
-    {}
 
     /// \brief Start accepting incoming connections
     /// \param Listening interface
@@ -297,31 +206,35 @@ public:
     ///                     void (const Session & session)
     template<class Callback>
     void listen(const std::string & address, uint32_t port, Callback && on_accept_handler){
-        if(this->status_)
+        if(status_)
             return base::fail("server is already running");
 
         base::processor::get().add_listener(address, port,
                                             boost::bind(&session<true, ReqBody>::template on_accept<Callback>,
                                                         boost::placeholders::_1,
-                                                        this->resource_map_cb_p_,
-                                                        this->method_map_cb_p_,
+                                                        resource_map_cb_p_,
+                                                        method_map_cb_p_,
                                                         std::forward<Callback>(on_accept_handler)
                                                         )
                                             )->accept_start();
-        this->status_ = true;
+        status_ = true;
     }
 
 
-    typename router_base::ref Router() noexcept{
-        return static_cast<typename router_base::ref>(*this);
+    basic_r BasicRouter() noexcept{
+        return {};
     }
 
-    typename router_base::cref Router() const noexcept{
-        return static_cast<typename router_base::ref>(*this);
+    chain_r ChainRouter() noexcept{
+        return {};
     }
 
-    typename router_base::ref use(const resource_regex_t & path_to_resource, typename router_base::cref other){
-        return router_base::use(path_to_resource, other);
+    void use(const resource_regex_t & path_to_resource, typename basic_r::cref other){
+        get_basic_router()->use(path_to_resource, other);
+    }
+
+    void use(const resource_regex_t & path_to_resource, typename chain_r::cref other){
+        get_chain_router()->use(path_to_resource, other);
     }
 
 }; // server_impl class
