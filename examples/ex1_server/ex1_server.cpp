0ce7749511d007924c501ca3e8bd529ceea8845b
0xdead4ead <steamtixonow26@gmail.com>
2018-08-15 15:10:54 +0500

    ex1_server.cpp update

--- examples/ex1_server/ex1_server.cpp
+++ examples/ex1_server/ex1_server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <server.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 
@@ -33,7 +34,19 @@ int main()
 //    root@x0x0:~# curl localhost --request 'GET' --request-target '/1'
 //    root@x0x0:~# curl localhost --request 'GET' --request-target '/2'
 //    root@x0x0:~# curl localhost --request 'GET' --request-target '/a/b/c/d'
+//    root@x0x0:~# curl localhost --request 'GET' --request-target '/user/1992'
     http::server my_http_server;
+
+    my_http_server.get("/user/\\d+",
+       [](auto & req, auto & session, auto & next){
+        // process '/user'
+        (void)req; (void)session; next();
+    }, [](auto & req, auto & session){
+        // process /id
+        auto target = req.target();
+        const auto user_id = boost::lexical_cast<int>(target.data() + 1);
+        session.do_write(make_response(req, std::to_string(user_id)));
+    });
 
     my_http_server.get("/1", [](auto & req, auto & session){
        cout << req << endl; // '/1'
