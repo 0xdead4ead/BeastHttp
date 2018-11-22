cf63b7e7719151a6d7f8807a9169fcb294e2d2ce
0xdead4ead <steamtixonow26@gmail.com>
2018-11-22 22:19:35 +0300

    remove extra includes

--- include/param.hpp
+++ include/param.hpp
@@ -1,23 +1,11 @@
 #ifndef BEAST_HTTP_PARAM_HPP
 #define BEAST_HTTP_PARAM_HPP
 
+#include <boost/lexical_cast.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/regex.hpp>
 #include <list>
 #include <memory>
-#include <vector>
-#include <boost/lexical_cast.hpp>
-#include "session.hpp"
-
-//#if __cplusplus >= 201703L
-//#include <variant>
-//template<class... Types>
-//using variant = std::variant<Types...>;
-//#else
-//#include <boost/variant.hpp>
-//template<class... Types>
-//using variant = boost::variant<Types...>;
-//#endif
-
-constexpr int max_param_args = 9;
 
 namespace http {
 
@@ -123,11 +111,16 @@ struct arguments<T1, T2, T3, T4, T5, T6, T7, T8, T9>{
         : _1{t1}, _2{t2}, _3{t3}, _4{t4}, _5{t5}, _6{t6}, _7{t7}, _8{t8}, _9{t9} {}
 };
 
-template<class ReqBody>
+template<class>
 class basic_router;
 
-template<class ReqBody>
+template<class>
 class chain_router;
+
+template<class, class>
+class list_cb;
+
+using resource_regex_t = std::string;
 
 /// \brief Split regex path to '/' character
 #define IMPL_SPLIT_REGEX \
