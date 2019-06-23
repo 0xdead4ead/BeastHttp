ddd2049cdf40ec0fc398c3d56beb730aedb700f2
0xdead4ead <steamtixonow26@gmail.com>
2019-06-23 18:25:02 +0300

    set ver to 24

--- BeastHttp/include/http/base/version.hxx
+++ BeastHttp/include/http/base/version.hxx
@@ -1,5 +1,5 @@
 #define BEASTHTTP_VERSION_MAJOR 2
-#define BEASTHTTP_VERSION_MINOR 3
+#define BEASTHTTP_VERSION_MINOR 4
 
 #define BEASTHTTP_VERSION_VALUE ((BEASTHTTP_VERSION_MAJOR * 10) \
                                                      + BEASTHTTP_VERSION_MINOR)
