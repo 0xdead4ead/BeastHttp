c3881006c4a1dedd3b62244880023a3e810d42c1
0xdead4ead <steamtixonow26@gmail.com>
2019-07-14 21:02:45 +0300

    version conf

---
+++ BeastHttp/include/http/base/version.hxx
@@ -0,0 +1,5 @@
+#define BEASTHTTP_VERSION_MAJOR 2
+#define BEASTHTTP_VERSION_MINOR 5
+
+#define BEASTHTTP_VERSION_VALUE ((BEASTHTTP_VERSION_MAJOR * 10) \
+                                                     + BEASTHTTP_VERSION_MINOR)
